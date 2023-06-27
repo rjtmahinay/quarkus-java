db3c25390961986eef3cdcc6a276bc80b3330de8
rjtmahinay <rjt.mahinay@gmail.com>
2023-06-27 16:47:32 +0800

    properties

--- src/main/java/com/rjtmahinay/resource/EmployeeResource.java
+++ src/main/java/com/rjtmahinay/resource/EmployeeResource.java
@@ -1,5 +1,6 @@
 package com.rjtmahinay.resource;
 
+import com.rjtmahinay.config.EmployeeProperties;
 import com.rjtmahinay.entity.Employee;
 import com.rjtmahinay.model.EmployeeDTO;
 import com.rjtmahinay.repository.EmployeeRepository;
@@ -8,35 +9,62 @@ import io.quarkus.cache.CacheKey;
 import io.quarkus.cache.CacheName;
 import io.quarkus.cache.CacheResult;
 import io.quarkus.cache.runtime.UndefinedCacheKeyGenerator;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Any;
+import jakarta.enterprise.inject.Default;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.cache.Cache;
 
 import java.util.List;
 
 @Path("/employee")
+@ApplicationScoped
 public class EmployeeResource {
     private static final Logger LOG = Logger.getLogger(EmployeeResource.class);
 
     @Inject
     EmployeeRepository employeeRepository;
 
+    @Inject
+    EmployeeProperties employeeProperties;
+
+    @ConfigProperty(name = "employee.name")
+    String employeeName;
+
+    @GET
+    @Path("/property")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<String> getEmployeeProperty() {
+        LOG.info("GET AN EMPLOYEE PROPERTY");
+        return Uni.createFrom().item(employeeProperties.name());
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @CacheResult(cacheName = "employee-cache")
     public List<Employee> getEmployee() {
         LOG.info("GET EMPLOYEE");
         return employeeRepository.findAll().list();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @CacheResult(cacheName = "employee-cache")
+    public Employee getEmployee(@PathParam("id") Long id) {
+        LOG.infof("GET EMPLOYEE by ID: %d", id);
+        return employeeRepository.findById(id);
     }
 
     @POST
     @Path("/add")
     @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
-    @CacheInvalidate(cacheName = "employee-cache")
     public void addEmployee(@CacheKey EmployeeDTO employeeDTO) {
         LOG.info("ADD EMPLOYEE");
         Employee employee = new Employee();
@@ -50,8 +78,9 @@ public class EmployeeResource {
     @PUT
     @Path("/update/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
+    @CacheInvalidate(cacheName = "employee-cache")
     @Transactional
-    public void updateEmployee(@PathParam("id") Long id, EmployeeDTO employeeDTO) {
+    public void updateEmployee(@CacheKey @PathParam("id") Long id, EmployeeDTO employeeDTO) {
         LOG.info("UPDATE EMPLOYEE");
         Employee employee = employeeRepository.findById(id);
         employee.name = employeeDTO.name();
@@ -62,6 +91,7 @@ public class EmployeeResource {
 
     @DELETE
     @Path("/delete/{id}")
+//    @CacheInvalidate(cacheName = "employee-cache")
     @Transactional
     public void deleteEmployee(@PathParam("id") Long id) {
         LOG.info("DELETE EMPLOYEE");
